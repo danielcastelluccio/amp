9430afa14f89d88dc734450f271f28462d51f1b9
danterusdev <danterusdev@gmail.com>
2021-10-04 20:20:15 -0700

    not functional, but some progress is made at getting a concat function running

--- programming_language.py
+++ programming_language.py
@@ -12,9 +12,6 @@ class Token:
 class Program(Token):
     def __init__(self, tokens):
         self.tokens = tokens
-
-    def __repr__(self):
-        return "program: " + str(self.tokens)
     
 class Function(Token):
     def __init__(self, name, tokens, locals):
@@ -22,15 +19,9 @@ class Function(Token):
         self.tokens = tokens
         self.locals = locals
     
-    def __repr__(self):
-        return "function : " + self.name + " : " + str(self.tokens)
-    
 class Statement(Token):
     def __init__(self, instructions):
         self.instructions = instructions
-    
-    def __repr__(self):
-        return "statement: " + str(self.instructions)
 
 class Instruction():
     pass
@@ -38,51 +29,38 @@ class Instruction():
 class Declare(Instruction):
     def __init__(self, name):
         self.name = name
-
-    def __repr__(self):
-        return "declare : " + self.name
     
 class Assign(Instruction):
     def __init__(self, name):
         self.name = name
-
-    def __repr__(self):
-        return "assign : " + self.name
     
 class Retrieve(Instruction):
     def __init__(self, name):
         self.name = name
-
-    def __repr__(self):
-        return "retrieve : " + self.name
     
 class Constant(Instruction):
     def __init__(self, value):
         self.value = value
     
-    def __repr__(self):
-        return "constant : " + str(self.value)
-    
 class Invoke(Instruction):
     def __init__(self, name):
         self.name = name
-    
-    def __repr__(self):
-        return "invoke : " + self.name
     
 class Return(Instruction):
     def __init__(self):
         pass
     
-    def __repr__(self):
-        return "return"
-    
 class Raw(Instruction):
     def __init__(self, instruction):
         self.instruction = instruction
     
-    def __repr__(self):
-        return "raw : " + self.instruction
+class Push(Instruction):
+    def __init__(self):
+        pass
+
+class Pop(Instruction):
+    def __init__(self):
+        pass
     
 def parse_file(file):
     file = open(file, "r")
@@ -120,9 +98,10 @@ def parse(contents, type):
 
         statements.append(Statement([Declare("stack"), Assign("stack")]))
 
-        for parameter in contents[contents.index("(") + 1 : contents.index(")")].split(","):
+        parameters = contents[contents.index("(") + 1 : contents.index(")")].split(",")
+        for parameter in parameters[::-1]:
             if parameter:
-                name = parameter.split(" ")[1]
+                name = parameter.lstrip().split(" ")[1]
                 statements.append(Statement([Declare(name), Assign(name)]))
         
         statements.append(Statement([Retrieve("stack")]))
@@ -154,8 +133,6 @@ def parse_statement(contents):
     contents = contents.lstrip()
     instructions = []
 
-    asm_instructions = ["push", "pop", "cld", "repne", "not", "add", "syscall"]
-
     if contents.startswith("variable "):
         name = contents.split(" ")[1]
         instructions.append(Declare(name))
@@ -165,11 +142,6 @@ def parse_statement(contents):
             expression = expression.lstrip()
             instructions.extend(parse_statement(expression))
             instructions.append(Assign(name))
-    elif contents.find("(") != -1:
-        inside_parenthesis = contents[contents.index("(") + 1 : contents.rindex(")")]
-        if inside_parenthesis:
-            instructions.extend(parse_statement(inside_parenthesis))
-        instructions.append(Invoke(contents[0 : contents.index("(")]))
     elif contents.startswith("return "):
         instructions.append(Assign("stack"))
         instructions.extend(parse_statement(contents[7 : len(contents)]))
@@ -179,8 +151,19 @@ def parse_statement(contents):
         instructions.append(Constant(int(contents)))
     elif contents.startswith("\""):
         instructions.append(Constant(contents[1 : len(contents) - 1]))
-    elif contents.split(" ")[0] in asm_instructions:
-        instructions.append(Raw(contents))
+    elif contents.startswith("asm "):
+        instructions.append(Raw(contents[4: len(contents)]))
+    elif contents.startswith("push "):
+        instructions.extend(parse_statement(contents[5 : len(contents)]))
+        instructions.append(Push())
+    elif contents.startswith("pop"):
+        instructions.append(Pop())
+    elif contents.find("(") != -1:
+        inside_parenthesis = contents[contents.index("(") + 1 : contents.rindex(")")].split(",")
+        for parameter in inside_parenthesis:
+            if parameter:
+                instructions.extend(parse_statement(parameter.lstrip()))
+        instructions.append(Invoke(contents[0 : contents.index("(")]))
     else:
         instructions.append(Retrieve(contents))
     return instructions
@@ -203,20 +186,24 @@ def create_asm(program, file_name_base):
             self.value = value
     
     asm_program = AsmProgram([], [])
-
-    asm_program.functions.append(AsmFunction("_start", [
-        "call main",
-        "mov rax, 60",
-        "xor rdi, rdi",
-        "syscall"
-    ]))
     
-    locals = ["r8", "r9", "r10", "r11"]
+    locals = ["r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"]
 
     for token in program.tokens:
         if isinstance(token, Function):
             asm_function = AsmFunction(token.name, [])
             for statement in token.tokens:
+                call_present = False
+                for instruction in statement.instructions:
+                    if isinstance(instruction, Invoke):
+                        call_present = True
+                
+                if call_present:
+                    for local in locals:
+                        asm_function.instructions.append("push " + local)
+                        
+                locals_popped = []
+
                 for instruction in statement.instructions:
                     if isinstance(instruction, Constant):
                         if isinstance(instruction.value, int):
@@ -224,38 +211,25 @@ def create_asm(program, file_name_base):
                         elif isinstance(instruction.value, str):
                             letters = string.ascii_lowercase
                             name = ( ''.join(random.choice(letters) for i in range(8)) )
-                            asm_program.data.append(AsmData(name, "\"" + instruction.value + "\""))
+                            asm_program.data.append(AsmData(name, "\"" + instruction.value + "\", 0x00"))
                             asm_function.instructions.append("push " + name)
                     elif isinstance(instruction, Invoke):
-                        if instruction.name == "push":
-                            pass
-                        elif instruction.name == "system_call":
-                            asm_function.instructions.append("syscall")
-                        elif instruction.name.startswith("pop_"):
-                            asm_function.instructions.append("pop " + instruction.name.split("_")[1])
-                        elif instruction.name.startswith("push_"):
-                            asm_function.instructions.append("push " + instruction.name.split("_")[1])
-                        elif instruction.name.startswith("mov_"):
-                            asm_function.instructions.append("mov " + instruction.name.split("_")[1])
-                        elif instruction.name == "cld":
-                            asm_function.instructions.append("cld")
-                        elif instruction.name == "repne_scasb":
-                            asm_function.instructions.append("repne scasb")
-                        elif instruction.name.startswith("not_"):
-                            asm_function.instructions.append("not " + instruction.name.split("_")[1])
-                        elif instruction.name.startswith("add_"):
-                            asm_function.instructions.append("add " + instruction.name.split("_")[1] + ", " + instruction.name.split("_")[2])
-                        elif instruction.name == "return":
-                            asm_function.instructions.append("ret")
-                        else:
-                            asm_function.instructions.append("call " + instruction.name)
+                        asm_function.instructions.append("call " + instruction.name)
                     elif isinstance(instruction, Raw):
                         asm_function.instructions.append(instruction.instruction)
                     elif isinstance(instruction, Assign):
                         asm_function.instructions.append("pop " + locals[token.locals.index(instruction.name)])
+                        locals_popped.append(locals[token.locals.index(instruction.name)])
                     elif isinstance(instruction, Retrieve):
                         asm_function.instructions.append("push " + locals[token.locals.index(instruction.name)])
                         
+                if call_present:
+                    for local in locals[::-1]:
+                        if not local in locals_popped:
+                            asm_function.instructions.append("pop " + local)
+                        else:
+                            asm_function.instructions.append("pop rax")
+
             asm_function.instructions.append("ret")
             asm_program.functions.append(asm_function)
 
